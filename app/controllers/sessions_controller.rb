c32673e6750bce6294b6e89af9f73ac9847cccac
Gerald Oliva <ryumaster001@yahoo.com>
2017-02-05 00:10:14 -0500

    Finished section 7

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,13 @@
 class SessionsController < ApplicationController
   def new
-    
   end
 
   def create
     if user = User.authenticate(params[:email], params[:username], params[:password])
       session[:user_id] = user.id
-      redirect_to user, notice: "Welcome back, #{user.name}!"
+      flash[:notice] = "Welcome back, #{user.name}!"
+      redirect_to(session[:intended_url] || user)
+      session[:intended_url] = nil
     else
       flash.now[:alert] = "Invalid email/password combination!"
       render :new
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170131031024) do
+ActiveRecord::Schema.define(version: 20170205041940) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -41,9 +41,10 @@ ActiveRecord::Schema.define(version: 20170131031024) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "username"
+    t.boolean  "admin",           default: false
   end
 
 end
